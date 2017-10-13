71749cbe01fa9136041584ad305282d20577cb29
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-10-13 15:29:20 -0400

    Add function to wipe index_blocks

---
+++ deleteblocks.py
@@ -0,0 +1,16 @@
+"""Utility command to clear SPIMI index blocks"""
+import shutil
+import os
+
+def deleteblocks():
+    """ Delete directories and files under ./index_blocks"""
+    for root, directories, files in os.walk('./index_blocks'):
+        for f in files:
+			      os.unlink(os.path.join(root, f))
+        for directory in directories:
+			      shutil.rmtree(os.path.join(root, directory))
+
+if __name__ == '__main__':
+    print('Deleting existing SPIMI blocks...')
+    deleteblocks()
+    print('Complete!')
